3b4b19432cde19585791b6e9877d01f62b19fd24
Daniel Ye <danielxinxin1@gmail.com>
2022-01-10 21:34:34 -0300

    initial commit

---
+++ src/main/java/com/example/bahamas/BahamasApplication.java
@@ -0,0 +1,13 @@
+package com.example.bahamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahamasApplication.class, args);
+	}
+
+}
